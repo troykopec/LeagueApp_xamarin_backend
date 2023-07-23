01b7b681018738e352f27a3dea52e601b19f8004
Troy Kopec <troykopec@gmail.com>
2023-07-23 19:36:32 -0400

    Add item model and fix sample controller to return item obj

--- LeagueApp_xamarin_backend/Controllers/SampleController.cs
+++ LeagueApp_xamarin_backend/Controllers/SampleController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using LeagueApp_xamarin_backend.Models; // Import the Item model from the Models folder
 
 namespace LeagueApp_xamarin_backend.Controllers
 {
@@ -7,24 +8,29 @@ namespace LeagueApp_xamarin_backend.Controllers
     [Route("api/[controller]")]
     public class SampleController : ControllerBase
     {
-        private readonly List<string> _data;
+        private readonly List<Item> _data; // Use List<Item> instead of List<string>
 
         public SampleController()
         {
             // Sample data for demonstration purposes
-            _data = new List<string> { "Item 1", "Item 2", "Item 3" };
+            _data = new List<Item>
+            {
+                new Item { Id = "1", Text = "Item 1", Description = "Description for Item 1" },
+                new Item { Id = "2", Text = "Item 2", Description = "Description for Item 2" },
+                new Item { Id = "3", Text = "Item 3", Description = "Description for Item 3" }
+            };
         }
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(_data);
+            return Ok(_data); // Return the list of Item objects
         }
 
         [HttpPost]
         public IActionResult Post([FromBody] string item)
         {
-            _data.Add(item);
+            _data.Add(new Item { Id = _data.Count.ToString(), Text = item, Description = "Description for " + item }); // Add a new Item object
             return Ok();
         }
     }
