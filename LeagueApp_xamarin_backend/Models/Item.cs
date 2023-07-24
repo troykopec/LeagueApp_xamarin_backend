35c85266ff3e573390d4686be9d419c8c9f3fa47
Troy Kopec <troykopec@gmail.com>
2023-07-23 23:10:03 -0400

    add database components

--- LeagueApp_xamarin_backend/Models/Item.cs
+++ LeagueApp_xamarin_backend/Models/Item.cs
@@ -1,11 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LeagueApp_xamarin_backend.Models
 {
     public class Item
     {
+        [Key]
         public string Id { get; set; }
+
+        [MaxLength(50)] // Set the maximum length to 50 characters
         public string Text { get; set; }
+
+        [Index(IsUnique = true)] // Set a unique constraint on the Description property
         public string Description { get; set; }
     }
 }
