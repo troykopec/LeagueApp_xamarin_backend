bcb979cae1915ef6b97cfc7b44aa08d5cb8d3f7a
Troy Kopec <troykopec@gmail.com>
2023-07-25 22:23:36 -0400

    Connect azure database to code in local vs code

---
+++ LeagueApp_xamarin_backend/Migrations/20230726021024_Initial0.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeagueApp_xamarin_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial0 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_Description",
+                table: "Items",
+                column: "Description",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LeagueApp_xamarin_backend
 backend functions for xamarin mobile app
+--------------------------------------------
+to update and migrate new database tables and changes, make the changes in MyDbContext.cs, I will leave items table there for an example. It needs to refer to a class, in the item tables case it is the object class "Item.cs".
+
+to apply changes (only works locally for now) go into vs code terminal and type :
+- dotnet ef migrations add <Name of Migration> --project LeagueApp_xamarin_backend.csproj --startup-project LeagueApp_xamarin_backend.csproj  
+- dotnet ef database update --project LeagueApp_xamarin_backend.csproj --startup-project LeagueApp_xamarin_backend.csproj 
