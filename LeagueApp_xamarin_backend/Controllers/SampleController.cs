9bf061a1263523c44ef5334f2f89522633ea7104
Troy Kopec <troykopec@gmail.com>
2023-07-26 22:31:40 -0400

    test 2

--- LeagueApp_xamarin_backend/Controllers/SampleController.cs
+++ LeagueApp_xamarin_backend/Controllers/SampleController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using LeagueApp_xamarin_backend.Models; // Import the Item model from the Models folder
+using LeagueApp_xamarin_backend.Data;
+using Microsoft.Extensions.Logging;
 
 namespace LeagueApp_xamarin_backend.Controllers
 {
@@ -9,22 +11,28 @@ namespace LeagueApp_xamarin_backend.Controllers
     public class SampleController : ControllerBase
     {
         private readonly List<Item> _data; // Use List<Item> instead of List<string>
+        
+        private readonly MyDbContext _dbContext;
+        private readonly ILogger<SampleController> _logger;
 
-        public SampleController()
+        public SampleController(MyDbContext dbContext, ILogger<SampleController> logger)
         {
-            // Sample data for demonstration purposes
-            _data = new List<Item>
-            {
-                new Item { Id = "1", Text = "Item 1", Description = "Description for Item 1" },
-                new Item { Id = "2", Text = "Item 2", Description = "Description for Item 2" },
-                new Item { Id = "3", Text = "Item 3", Description = "Description for Item 3" }
-            };
+            _dbContext = dbContext;
+            _logger = logger;
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public ActionResult<IEnumerable<Item>> GetItems()
         {
-            return Ok(_data); // Return the list of Item objects
+            var items = _dbContext.GetItems(); // Call the GetItems method from your MyDbContext
+            _logger.LogInformation("Calling GetItems()");
+            
+            if (items != null)
+            {
+                return Ok(items);
+            }
+
+            return NotFound();
         }
 
         [HttpPost]
