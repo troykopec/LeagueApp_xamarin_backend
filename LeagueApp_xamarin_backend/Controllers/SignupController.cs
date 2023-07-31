5bf7d28a7f04becc462d9269e1a1a85142afc2cf
Troy Kopec <troykopec@gmail.com>
2023-07-30 22:21:02 -0400

    Update SignupController.cs

--- LeagueApp_xamarin_backend/Controllers/SignupController.cs
+++ LeagueApp_xamarin_backend/Controllers/SignupController.cs
@@ -23,16 +23,10 @@ namespace LeagueApp_xamarin_backend.Controllers
         [HttpPost]
         public IActionResult Signup([FromBody] User user)
         {
-                // Additional check: If the username is not null or empty, return BadRequest
-            if (!string.IsNullOrEmpty(user.Username))
-            {
-                _logger.LogError("Username should not be provided during signup.");
-                return BadRequest("Username should not be provided during signup.");
-            }
             
-            if (user == null)
+            if (!(user == null))
             {
-                return BadRequest("User data is null.");
+                return BadRequest("User data is not null.");
             }
 
             // Set the CreatedAt and UpdatedAt properties to the current datetime
