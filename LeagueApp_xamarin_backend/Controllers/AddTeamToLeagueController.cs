b149db1f16c98476fe75dc1931ac56ca18a09b6b
Troy Kopec <troykopec@gmail.com>
2023-10-03 21:47:56 -0400

    Create AddTeamToLeagueController.cs

---
+++ LeagueApp_xamarin_backend/Controllers/AddTeamToLeagueController.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using LeagueApp_xamarin_backend.Data;
+using LeagueApp_xamarin_backend.Models;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using Newtonsoft.Json;
+
+namespace LeagueApp_xamarin_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AddTeamToLeagueController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+        private readonly ILogger<AddTeamToLeagueController> _logger;
+
+        public AddTeamToLeagueController(MyDbContext context, ILogger<AddTeamToLeagueController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> AddTeam([FromBody] Team model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var response = new ApiResponse();
+
+                var userIdString = User.FindFirstValue("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier");        
+                var username = User.FindFirstValue(JwtRegisteredClaimNames.UniqueName);
+
+                if (!string.IsNullOrEmpty(userIdString) && int.TryParse(userIdString, out int userId))
+                {
+                    // Create a new League object based on the input model
+                    var newTeam = new Team
+                    {
+                        TeamName = model.TeamName,
+                        LeagueId = model.LeagueId,
+                        Players = model.Players,
+                        TeamLeaderId = userId,
+                    };
+
+                    // Add the new team to the database
+                    _context.AddTeamToLeague(newTeam);
+
+                    // ...
+                    response.Message = $"team addition Successful. team Details: {JsonConvert.SerializeObject(newTeam)}";
+                }
+                else
+                {
+                    // Handle the case where userIdString is null or not a valid integer
+                    response.Message = $"Error: Invalid user ID. id: {userIdString}";
+                }  
+                // Return a success response
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while creating a team.");
+                return StatusCode(500, "An error occurred while creating a team.");
+            }
+        }
+    }
+}
