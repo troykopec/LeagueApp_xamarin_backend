09ac031a68e9c19911b2b80e3eaaa311b9e4f16a
Troy Kopec <troykopec@gmail.com>
2023-12-09 17:23:13 -0500

    no unique code

--- LeagueApp_xamarin_backend/Program.cs
+++ LeagueApp_xamarin_backend/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using LeagueApp_xamarin_backend.Data;
-using LeagueApp_xamarin_backend.Helpers;
 using LeagueApp_xamarin_backend.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
@@ -54,9 +53,6 @@ builder.Services.AddAuthentication(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Register the UniqueCodeGenerator
-builder.Services.AddScoped<UniqueCodeGenerator>();
 
 var app = builder.Build();
 
